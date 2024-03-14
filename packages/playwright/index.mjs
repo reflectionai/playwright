63bafc22f5c44e0c905f5e5ab2a87413cc328c00
ethanabrooks <ethanabrooks@gmail.com>
2024-03-14 11:55:14 -0400

    export codegen from index.mjs

--- packages/playwright/index.mjs
+++ packages/playwright/index.mjs
@@ -13,6 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export * from 'playwright-core';
-import playwright from 'playwright-core';
+export * from "playwright-core";
+import playwright from "playwright-core";
 export default playwright;
